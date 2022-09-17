953c5af0846e11b8f6d07e80ce3400cbead93fe2
G1 <g1.tommy.jeon@gmail.com>
2022-09-17 21:14:41 +0900

    feat: Adds MSW

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import './app.css'
 import App from './App.svelte'
 
+if (import.meta.env.MODE === "development") {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
 const app = new App({
   target: document.getElementById('app')
 })
